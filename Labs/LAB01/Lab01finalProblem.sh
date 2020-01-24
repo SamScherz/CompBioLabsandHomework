8af1aea002a2a60f2a48ea610057f017a4e76217
SamScherz <scherzsam@gmail.com>
2020-01-24 15:19:39 -0800

    Changed the store to text file option

--- Labs/LAB01/Lab01finalProblem.sh
+++ Labs/LAB01/Lab01finalProblem.sh
@@ -1,1 +1,1 @@
-cut -f 1, -d, PredPreyData.csv > PredPreyDataFixed.csv | tail PredPreyData.csv >> PredPreyDataFixed.csv | head -n 1 PredPreyData.csv >> PredPreyDataFixed.csv
+cut -f 1, -d, PredPreyData.csv > PredPreyDataFixed.csv |  head -n 1 PredPreyData.csv >> PredPreyDataFixed.csv | tail PredPreyData.csv >>> PredPreyDataFixed.csv |
