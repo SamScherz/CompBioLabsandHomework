225218ae4380d6602a59ba7aa98303f042879c68
SamScherz <scherzsam@gmail.com>
2020-01-24 15:51:28 -0800

    The Code WORKS!

--- Labs/LAB01/Lab01finalProblem.sh
+++ Labs/LAB01/Lab01finalProblem.sh
@@ -1,1 +1,2 @@
-cut -f 1, -d, PredPreyData.csv > PredPreyDataFixed.csv |  head -n 1 PredPreyData.csv >> PredPreyDataFixed.csv | tail PredPreyData.csv >>> PredPreyDataFixed.csv |
+head -n 1 PredPreyData.csv > PredPreyDataFixed.csv | tail PredPreyData.csv > PredPreyDataFixed.csv | cut -f 2-4 -d, PredPreydata.csv > PredPreyDataFixed.csv
+
